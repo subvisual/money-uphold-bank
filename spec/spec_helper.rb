3520aa13f081027e08466257e66ae714d12a64fd
Miguel Palhas <mpalhas@gmail.com>
2016-06-16 14:58:12 +0100

    All green

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ RSpec.configure do |config|
       stub_request(:get, "https://api.uphold.com/v0/ticker").
       to_return(
         status: 200,
-        body: File.read(File.join(File.dirname(__FILE__), "fixtures", "ticker.json"))
+        body: File.read(File.join(File.dirname(__FILE__), "fixtures", "ticker.json")),
       )
   end
 end
